85184b4ca8ffb3630e2e737327ddf424e67c7e46
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-11-26 16:32:30 -0500

    pages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,10 +4,10 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '#', current: true },
+  { name: 'Pricing', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
+  { name: 'About Us', href: '#', current: false },
 ]
 
 function classNames(...classes) {
