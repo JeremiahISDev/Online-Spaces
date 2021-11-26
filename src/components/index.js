2822c5f71afc3ad47eac9f2d81437d4e123edc49
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-11-26 17:04:14 -0500

    pages

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navigation } from "./Navigation.jsx";
+export { default as Footer } from "./Footer.jsx";
+export { default as Home } from "./Home.jsx";
+export { default as About } from "./About.jsx";
+export { default as Contact } from "./Contact.jsx";
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navigation, Footer, Home, About, Contact } from "./components";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
