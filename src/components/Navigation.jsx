73ff37ee14aea216c84fdce1061a6db142cc21d4
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-11-29 18:42:37 -0500

    nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,23 +6,23 @@ export default function Example() {
 					<div class="flex items-center justify-between h-16">
 						<div class="w-full justify-between flex items-center">
 							<a class="flex-shrink-0" href="/">
-								<img class="h-8 w-8" src="/icons/rocket.svg" alt="Workflow" />
+								<img class="h-15 w-15" src="../assets/Logo.png" alt="Online Spaces" />
 							</a>
 							<div class="hidden md:block">
 								<div class="ml-10 flex items-baseline space-x-4">
-									<a class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">
+									<a class="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/#">
 										Home
                             </a>
 									<a class="text-gray-800 dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/Pricing">
 										Pricing
                             </a>
-									<a class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/contact">
+									<a class="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/contact">
 										Contact
                             </a>
-									<a class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/aboutus">
+									<a class="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/aboutus">
 										About Us
                             </a>
-									<a class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/try">
+									<a class="text-gray-800  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="/try">
 										<button class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200">
 											Try One Free Meeting!
                                 </button>
@@ -35,7 +35,7 @@ export default function Example() {
 							</div>
 						</div>
 						<div class="-mr-2 flex md:hidden">
-							<button class="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+							<button class="text-gray-800 dark:text-white hover:text-gray-800 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
 								<svg width="20" height="20" fill="currentColor" class="h-8 w-8" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
 									<path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
 									</path>
@@ -46,16 +46,16 @@ export default function Example() {
 				</div>
 				<div class="md:hidden">
 					<div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-						<a class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/">
+						<a class="text-gray-800 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/">
 							Home
                 </a>
 						<a class="text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
 							Gallery
                 </a>
-						<a class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
+						<a class="text-gray-800 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
 							Content
                 </a>
-						<a class="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
+						<a class="text-gray-800 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
 							Contact
                 </a>
 					</div>
