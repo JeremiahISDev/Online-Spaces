537523d2ea5562df94acc470482de0a1ef142345
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-12-01 23:15:08 +0000

    footer.jsx

--- src/components/Pricing Cards/index.js
+++ src/components/Pricing Cards/index.js
@@ -1,3 +1,4 @@
 export { default as Free } from "./Free";
 export { default as Premium } from "./Premium";
 export { default as Enterprise } from "./Enterprise";
+export { default as PersonalPro } from "./PersonalPro";
